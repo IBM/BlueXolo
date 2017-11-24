747452dc8afed459bdfa46662baa888fc79fe55d
Francisco Suárez <franksg88@gmail.com>
2017-11-23 20:48:58 -0600

    Run scripts views complete.

--- apps/Testings/urls.py
+++ apps/Testings/urls.py
@@ -5,7 +5,7 @@ from .views import KeyWordsView, NewKeywordView, DeleteKeywordView, CollectionsV
     DeleteCollectionsView, EditKeywordView, TestcaseView, NewTestcaseView, EditTestcaseView, DeleteTestcaseView, \
     TestsuiteView, \
     NewTestsuiteView, EditTestsuiteView, DeleteTestsuiteView, NewKeywordImportedView, EditKeywordImportedView, \
-    KeywordsImportedView, DeleteImportedScriptView
+    KeywordsImportedView, DeleteImportedScriptView, SearchRunScriptView
 
 urlpatterns = [
     url(r'^keywords/$', KeyWordsView.as_view(), name="keywords"),
@@ -32,4 +32,6 @@ urlpatterns = [
     url(r'^import/$', NewKeywordImportedView.as_view(), name="new-import-script"),
     url(r'^import/(?P<pk>\d+)/edit/$', EditKeywordImportedView.as_view(), name="edit-import-script"),
     url(r'^import/(?P<pk>\d+)/delete/$', DeleteImportedScriptView.as_view(), name="delete-import-script"),
+    # Run scripts
+    url(r'^run/$', SearchRunScriptView.as_view(), name="search-run-script"),
 ]
