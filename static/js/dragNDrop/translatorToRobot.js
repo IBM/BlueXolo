4a43842bbebc37ab585b5cd4d7748992130ce2d8
Isaac <a01227947@itesm.mx>
2017-12-21 17:17:47 -0600

    variable name fix!

--- static/js/dragNDrop/translatorToRobot.js
+++ static/js/dragNDrop/translatorToRobot.js
@@ -156,6 +156,9 @@ function handleKeywordSection(keywordName, customKeyword){
     var translatedRow = "\n";
 
     // Version 3
+    // ToDo
+    // Version 3 will handle a flag if the user wants to add the section or not.
+    // At this moment it will not add the section if the current object is not a keyword
     if(!isKeyword){
         translatedRow += "\t"+keywordName+"\n";
         keywordSection = true;
