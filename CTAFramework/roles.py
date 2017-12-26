b7a72f92b01cbb14d98c00100039f7b3200e1d87
Francisco Suarez <franksg88@gmail.com>
2017-12-26 11:26:04 -0600

    Added Owner roles (same develop) But require new flag for staff member

--- CTAFramework/roles.py
+++ CTAFramework/roles.py
@@ -45,17 +45,75 @@ class Developer(AbstractUserRole):
 
 class Tester(AbstractUserRole):
     available_permissions = {
-
+        # Test Case
+        'read_test_case': True,
+        # Test Suite
+        'read_test_suite': True,
+        # Collection
+        'read_collection': True,
+        # Imported Script
+        'read_imported_script': True,
+        # Scripts
+        'run_scripts': True,
     }
 
 
 class Auditor(AbstractUserRole):
     available_permissions = {
-        'create_server_profile': True,
+        # Profiles
+        'read_server_profile': True,
+        # Parameters
+        'read_server_parameter': True,
+        # Keywords
+        'read_keyword': True,
+        # Test Case
+        'read_test_case': True,
+        # Test Suite
+        'read_test_suite': True,
+        # Collection
+        'read_collection': True,
+        # Imported Script
+        'read_imported_script': True,
     }
 
 
 class Owner(AbstractUserRole):
     available_permissions = {
+        # Profiles
         'create_server_profile': True,
+        'read_server_profile': True,
+        'update_server_profile': True,
+        'delete_server_profile': True,
+        # Parameters
+        'create_server_parameter': True,
+        'read_server_parameter': True,
+        'update_server_parameter': True,
+        'delete_server_parameter': True,
+        # Keywords
+        'create_keyword': True,
+        'read_keyword': True,
+        'update_keyword': True,
+        'delete_keyword': True,
+        # Test Cases
+        'create_test_case': True,
+        'read_test_case': True,
+        'update_test_case': True,
+        'delete_test_case': True,
+        # Test suites
+        'create_test_suite': True,
+        'read_test_suite': True,
+        'update_test_suite': True,
+        'delete_test_suite': True,
+        # Imported Script
+        'create_imported_script': True,
+        'read_imported_script': True,
+        'update_imported_script': True,
+        'delete_imported_script': True,
+        # Collection
+        'create_collection': True,
+        'read_collection': True,
+        'update_collection': True,
+        'delete_collection': True,
+        # Scripts
+        'run_scripts': True,
     }
