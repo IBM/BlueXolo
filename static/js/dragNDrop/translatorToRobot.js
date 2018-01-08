880d53eeac5577d490300d62d0d77d410a0f3579
Isaac <a01227947@itesm.mx>
2018-01-08 11:01:48 -0600

    Testcases translation now adds its phase

--- static/js/dragNDrop/translatorToRobot.js
+++ static/js/dragNDrop/translatorToRobot.js
@@ -44,15 +44,14 @@ function checkIfExtraElementsExist(){
     }  
 }
 
-function addKeywordName(){
-    var keywordNameTextArea = document.getElementById("keyword_name");
-    var testcaseNameTextArea = document.getElementById("testcase_name");
-
-    if(keywordNameTextArea !== null){
+function addKeywordName(){        
+    if(isKeyword()){
+        var keywordNameTextArea = document.getElementById("keyword_name");
         translatedRow = keywordNameTextArea.value;
         return translatedRow;
     }
-    else if(testcaseNameTextArea !== null){
+    else if(isTestcase()){
+        var testcaseNameTextArea = document.getElementById("testcase_name");
         translatedRow = testcaseNameTextArea.value;
         return translatedRow;
     }
@@ -62,18 +61,25 @@ function addKeywordName(){
 }
 
 function addDocumentationSection(){
-    var translatedRow = "\t[Documentation]\t";
-
-    var keywordDescriptionTextArea = document.getElementById("keyword_description");
-    var testcaseDescriptionTextArea = document.getElementById("testcase_description");
+    var translatedRow = "\t[Documentation]\t";   
     
-    if(keywordDescriptionTextArea !== null){
+    if(isKeyword()){
+        var keywordDescriptionTextArea = document.getElementById("keyword_description");
         translatedRow += keywordDescriptionTextArea.value;
         translatedRow += "\n";
         return translatedRow;
     }
-    else if(testcaseDescriptionTextArea !== null){
+    else if(isTestcase()){
+        var testcaseDescriptionTextArea = document.getElementById("testcase_description");
         translatedRow += testcaseDescriptionTextArea.value;
+        translatedRow += "\n";
+
+        var testcasePhasesList = document.getElementById("phases_list");
+        var selectedOption = testcasePhasesList.options.selectedIndex;
+        var selectedValueText = testcasePhasesList.options[selectedOption].text;
+
+        translatedRow += "\t[Tags]\t"; 
+        translatedRow += selectedValueText;
         translatedRow += "\n";
         return translatedRow;
     }
