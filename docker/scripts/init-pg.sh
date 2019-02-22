542609d20f25a4ec5ba6beba1ecc5f72a054116f
Fernando Quintero Amador <quintero@quintero.gdl.mex.ibm.com>
2019-02-22 13:52:03 -0600

    Changes to Docker configuration

--- docker/scripts/init-pg.sh
+++ docker/scripts/init-pg.sh
@@ -12,5 +12,21 @@
 #
 
 if [ -e /FIRST_SETUP ]; then
+echo "First setup..."
+  if [ ! -e /var/lib/pgsql/data ]; then
+    echo "Postgres data dir does not exist, initializing"
+    mkdir -p /var/lib/pgsql/data
+    chown -R postgres: /var/lib/pgsql
+  fi
   postgresql-setup initdb
+  systemctl restart postgresql
+  if [ ! -e /var/lib/pgsql/BLUEXOLO ]; then
+    echo "Setting up BlueXolo Data"
+    su -l postgres -c  "psql < /update_pg_pwd.sql"
+    sed -i -e '{s/ident/md5/g}' /var/lib/pgsql/data/pg_hba.conf
+    sed -i -e '{s/peer/md5/g}' /var/lib/pgsql/data/pg_hba.conf
+    systemctl restart postgresql
+    su -l postgres -c "PGPASSWORD=bluexolo psql < /create_bluexolo_db.sql"
+    touch /var/lib/pgsql/BLUEXOLO
+  fi
 fi
