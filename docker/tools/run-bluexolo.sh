2e7f5e6889bbb4bbe01226a12fb764c5628352f9
Fernando Quintero Amador <quintero@quintero.gdl.mex.ibm.com>
2019-03-08 21:43:24 -0600

    Fixes for Linux

--- docker/tools/run-bluexolo.sh
+++ docker/tools/run-bluexolo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run bluexolo 
-
-docker run --privileged \
--ti -p 8000:8000 \
+nohup docker run --privileged \
+ -p 8000:8000 \
+ -h bluexolo \
 -e container=docker \
 -v /sys/fs/cgroup:/sys/fs/cgroup \
 -v /opt/sources/BlueXolo:/opt/BlueXolo-src \
 -v /opt/bluexolo-data:/var/lib/pgsql \
-bluexolo/bluexolo
+bluexolo/bluexolo  &
