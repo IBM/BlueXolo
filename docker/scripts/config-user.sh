ae2db05a42ce08db1b0cf351fd0a32a6f9a0a232
Santiago Valle <snvc00@gmail.com>
2020-11-04 11:20:16 -0600

    Add requirements to run robot scripts

--- docker/scripts/config-user.sh
+++ docker/scripts/config-user.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -
 
 # | - - - - - - - - - - - - - - - - - - - - - - - - - - -  |
-# |      Script for Robot Framework installation           |
-# |           and bluexolo user configurations             |
+# |        Script for bluexolo user configurations         |
 # |              Last update November 2020                 |
 # |                  by Santiago Valle                     |
 # | - - - - - - - - - - - - - - - - - - - - - - - - - - -  |
-
-# Install robot framework
-pip3 install robotframework robotframework-requests robotframework-selenium2library robotframework-sshlibrary
 
 # Set a password for the bluexolo user
 echo 'bluexolo:bluexolo' | chpasswd
