2418dab6f344083560c58d5799493e19930c4b73
Santiago Valle <snvc00@gmail.com>
2020-12-21 10:22:57 -0800

    Check dirs after run image

--- docker/tools/start-bluexolo.sh
+++ docker/tools/start-bluexolo.sh
@@ -2,7 +2,7 @@
 
 
 # Run bluexolo 
-nohup docker run --privileged \
+docker run --privileged -d \
  -p 8000:8000 \
  -h bluexolo \
 -e container=docker \
@@ -10,10 +10,9 @@ nohup docker run --privileged \
 -v /opt/sources/BlueXolo:/opt/BlueXolo-src \
 -v /opt/bluexolo-data:/var/lib/postgresql \
 --name bluexolo \
-bluexolo/bluexolo  &
+bluexolo/bluexolo
 
-# Configure bluexolo user
-docker exec -ti bluexolo bash -c "/opt/BlueXolo-src/docker/scripts/config-user.sh && /opt/BlueXolo-src/tools/check_dirs.sh"
+# Configure bluexolo user and check required directories
+docker exec -ti bluexolo bash -c "sleep 3 && /opt/BlueXolo-src/docker/scripts/config-user.sh && /opt/BlueXolo-src/tools/check_dirs.sh"
 
 exit 0
-
