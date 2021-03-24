924389c57c9c150cb8d325c413df5a077ccbac93
Santiago Valle <snvc00@gmail.com>
2021-03-24 16:15:52 -0600

    Disable commands where the database is modified, use gunicorn instead of runserver

--- docker/bluexolo-entrypoint.sh
+++ docker/bluexolo-entrypoint.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
 set -e
-cd ..
 
+PORT=${DJANGO_PORT:-8000}
+
+# IMPORTANT: These commands are commented until the team define who
+#            is going to run them and when.
+# ------------------------------------------------------------------------
 # Check for schema changes and apply them
-python3 manage.py makemigrations
-python3 manage.py migrate
+#python3 manage.py makemigrations
+#python3 manage.py migrate
 
-# Initialize robot
-python3 manage.py initialize_robot
+# Add superuser
+#python3 manage.py createsuperuser --email bluexolo@bluexolo.net --no-input
 
-#
+# Add variables to database
+#python3 manage.py run_base_migrations.py
+
+# For robot control flow sentences
+#python3 manage.py initialize_robot
 
 # Reset ID's to prevent duplications due legacy data in DB
-python3 manage.py sqlsequencereset Products Servers Testings Users
+#python3 manage.py sqlsequencereset Products Servers Testings Users
+# ------------------------------------------------------------------------
 
-# Run
-python3 manage.py runserver $DJANGO_IP:$DJANGO_PORT
+# Run (alternative to python3 manage.py runserver $DJANGO_IP:$DJANGO_PORT)
+gunicorn --bind :$PORT --workers 3 CTAFramework.wsgi:application
